426fbfe93344633d18be0fb343fd274043cc4b19
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-10 14:45:13 +0100

    BASE: Started Map examples

---
+++ 002-maps-loading.js
@@ -0,0 +1,11 @@
+// you can pass in key value pairs to the Map constructor
+const booksById = ([
+	[1, { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' }],
+	[2, { title: 'To Kill a Mockingbird', author: 'Harper Lee' }],
+	[3, { title: '1984', author: 'George Orwell' }],
+])
+
+// iterate over the entries
+for (const [id, book] of booksById) {
+	console.log(`ID: ${id}, Title: ${book.title}, Author: ${book.author}`);
+}
---
+++ 010-maps-basics.js
@@ -0,0 +1,12 @@
+const booksById = new Map();
+booksById.set(1, { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' });
+booksById.set(2, { title: 'To Kill a Mockingbird', author: 'Harper Lee' });
+
+console.log("has id 1?", booksById.has(1)); // Output: true
+console.log("has id 3?", booksById.has(3)); // Output: false
+
+// Accessing a book by its ID
+const book1 = booksById.get(1);
+console.log(book1); // Output: { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' }
+
+console.log(booksById.get(3)); // Output: undefined
